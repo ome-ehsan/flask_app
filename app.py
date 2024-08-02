f3f4aa7aabe0f1bea341ebcbf08934bfe82f7ac9
ome-ehsan <ome2ehsan001@gmail.com>
2024-08-02 22:16:53 +0600

    Add http GET ,POST and import redirect, request from flask lib

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
-#my app
+
+
+# my app
 app = Flask(__name__)
 Scss(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasksDatabase.db"
 db = SQLAlchemy(app)
-
-
+# databse models
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     task = db.Column(db.String(150), nullable = False )
@@ -18,15 +19,16 @@ class Tasks(db.Model):
     created = db.Column(db.DateTime, default = datetime.now)
     
     def __repr__(self) -> str:
-        return f"Task : {self.id}"
+        return f"Task {self.id}"
     
     
+   
+   
     
 # homepage of my app
-@app.route("/")
+@app.route("/", method = ["POST","GET"])  # http methods to add task to db and get tasks from db
 def index(): #homepage
     return render_template("index.html")
-
 
 
 if __name__ in "__main__":
