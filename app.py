e37456fcb2a7e5c6b4d620eb339130a32ac84745
ome-ehsan <ome2ehsan001@gmail.com>
2024-08-03 17:43:45 +0600

    Add functionalities to Delete and Update. Create update.html for update button

--- app.py
+++ app.py
@@ -21,10 +21,7 @@ class Tasks(db.Model):
     def __repr__(self) -> str:
         return f"Task {self.id}"
     
-    
    
-   
-    
 # homepage of my app
 @app.route("/", methods = ["POST","GET"])  # http methods to add task to db and get tasks from db
 def index(): #homepage
@@ -33,7 +30,7 @@ def index(): #homepage
     if request.method == "POST" :
         # gotta grab the task from 'form in index.html'
         grabbed_task = request.form["content"]
-        task_to_be_sent = Tasks(task = grabbed_task)
+        task_to_be_sent = Tasks(task = grabbed_task)  #line17
         try:
             db.session.add(task_to_be_sent)
             db.session.commit()
@@ -47,6 +44,37 @@ def index(): #homepage
         tasks = Tasks.query.order_by(Tasks.created).all()
         return render_template("index.html", tasks = tasks)
 
+
+@app.route("/delete/<int:id>")
+def delete(id:int): #as tasks are saved in db we gotta grab from the db hence the query
+    grabbed_task = Tasks.query.get_or_404(id)  
+    try:
+        db.session.delete(grabbed_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"ERROR : {e}"
+    
+    
+@app.route("/update/<int:id>",methods=["POST","GET"])
+def edit(id:int):
+    grabbed_task = Tasks.query.get_or_404(id)
+    if request.method == "POST":
+        grabbed_task.task = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR : {e}"
+    else:
+        return render_template("update.html" , task = grabbed_task)
+        
+            
+            
+            
+    
+    
+    
 
 if __name__ in "__main__":
     with app.app_context():
